19ec0454c6310f46ea9084f208095f4b8387e323
Yoselyn Cántaro <yoselynespinoza27@gmail.com>
2024-10-23 12:19:43 -0500

    Estructura Inicial del Proyecto

---
+++ src/main/java/pe/com/cibertec/ef_cantaro/EfCantaroApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.ef_cantaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfCantaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfCantaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/ef_cantaro/EfCantaroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.ef_cantaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfCantaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
